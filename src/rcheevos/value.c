690333a65506668381118fcff0925f097cf37b0a
Jamiras <32680403+Jamiras@users.noreply.github.com>
2019-09-08 07:24:35 -0600

    Add flag for tracking HitCount (#21)

--- src/rcheevos/value.c
+++ src/rcheevos/value.c
@@ -1,8 +1,79 @@
 #include "internal.h"
 
+static void rc_parse_cond_value(rc_value_t* self, const char** memaddr, rc_parse_state_t* parse) {
+  rc_condition_t** next;
+  int has_measured;
+
+  has_measured = 0;
+  self->expressions = 0;
+
+  /* this largely duplicates rc_parse_condset, but we cannot call it directly, as we need to check the 
+   * type of each condition as we go */
+  self->conditions = RC_ALLOC(rc_condset_t, parse);
+  self->conditions->next = 0;
+  self->conditions->has_pause = 0;
+
+  next = &self->conditions->conditions;
+  for (;;) {
+    *next = rc_parse_condition(memaddr, parse);
+
+    if (parse->offset < 0) {
+      return;
+    }
+
+    switch ((*next)->type) {
+      case RC_CONDITION_ADD_HITS:
+      case RC_CONDITION_ADD_SOURCE:
+      case RC_CONDITION_SUB_SOURCE:
+      case RC_CONDITION_AND_NEXT:
+        /* combining flags are allowed */
+        break;
+
+      case RC_CONDITION_RESET_IF:
+        /* ResetIf is allowed (primarily for rich presense - leaderboard will typically cancel instead of resetting) */
+        break;
+
+      case RC_CONDITION_MEASURED:
+        if (has_measured) {
+          parse->offset = RC_DUPLICATED_VALUE_MEASURED;
+          return;
+        }
+        has_measured = 1;
+        break;
+
+      default:
+        /* non-combinding flags and PauseIf are not allowed */
+        parse->offset = RC_INVALID_VALUE_FLAG;
+        return;
+    }
+
+    (*next)->pause = 0;
+    next = &(*next)->next;
+
+    if (**memaddr != '_') {
+      break;
+    }
+
+    (*memaddr)++;
+  }
+
+  *next = 0;
+
+  if (!has_measured) {
+    parse->offset = RC_MISSING_VALUE_MEASURED;
+  }
+}
+
 void rc_parse_value_internal(rc_value_t* self, const char** memaddr, rc_parse_state_t* parse) {
   rc_expression_t** next;
 
+  /* if it starts with a condition flag (M: A: B: C:), parse the conditions */
+  if ((*memaddr)[1] == ':') {
+    rc_parse_cond_value(self, memaddr, parse);
+    return;
+  }
+
+  self->conditions = 0;
   next = &self->expressions;
 
   for (;;) {
@@ -50,11 +121,23 @@ rc_value_t* rc_parse_value(void* buffer, const char* memaddr, lua_State* L, int 
   return parse.offset >= 0 ? self : 0;
 }
 
-int rc_evaluate_value(rc_value_t* self, rc_peek_t peek, void* ud, lua_State* L) {
+static int rc_evaluate_cond_value(rc_value_t* self, rc_peek_t peek, void* ud, lua_State* L) {
+  rc_condition_t* condition;
+  int reset;
+
+  rc_test_condset(self->conditions, &reset, peek, ud, L);
+
+  for (condition = self->conditions->conditions; condition != 0; condition = condition->next) {
+    if (condition->type == RC_CONDITION_MEASURED)
+      return rc_total_hit_count(self->conditions->conditions, condition);
+  }
+
+  return 0;
+}
+
+static int rc_evaluate_expr_value(rc_value_t* self, rc_peek_t peek, void* ud, lua_State* L) {
   rc_expression_t* exp;
   int value, max;
-
-  rc_update_memref_values(self->memrefs, peek, ud);
 
   exp = self->expressions;
   max = rc_evaluate_expression(exp, peek, ud, L);
@@ -68,4 +151,14 @@ int rc_evaluate_value(rc_value_t* self, rc_peek_t peek, void* ud, lua_State* L) 
   }
 
   return max;
+}
+
+int rc_evaluate_value(rc_value_t* self, rc_peek_t peek, void* ud, lua_State* L) {
+  rc_update_memref_values(self->memrefs, peek, ud);
+
+  if (self->expressions) {
+    return rc_evaluate_expr_value(self, peek, ud, L);
+  }
+
+  return rc_evaluate_cond_value(self, peek, ud, L);
 }
