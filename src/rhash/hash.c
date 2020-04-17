cb5b14f3fb916fb3289fda3a42a805a61f92aabb
Jamiras <jamirasmt@gmail.com>
2020-04-16 21:35:56 -0600

    hash whole file for intellivision

--- src/rhash/hash.c
+++ src/rhash/hash.c
@@ -791,6 +791,7 @@ int rc_hash_generate_from_buffer(char hash[33], int console_id, uint8_t* buffer,
     case RC_CONSOLE_GAMEBOY_ADVANCE:
     case RC_CONSOLE_GAMEBOY_COLOR:
     case RC_CONSOLE_GAME_GEAR:
+    case RC_CONSOLE_INTELLIVISION:
     case RC_CONSOLE_MASTER_SYSTEM:
     case RC_CONSOLE_MEGA_DRIVE:
     case RC_CONSOLE_NEOGEO_POCKET:
@@ -1002,6 +1003,7 @@ int rc_hash_generate_from_file(char hash[33], int console_id, const char* path)
     case RC_CONSOLE_GAMEBOY_ADVANCE:
     case RC_CONSOLE_GAMEBOY_COLOR:
     case RC_CONSOLE_GAME_GEAR:
+    case RC_CONSOLE_INTELLIVISION:
     case RC_CONSOLE_MASTER_SYSTEM:
     case RC_CONSOLE_MEGA_DRIVE:
     case RC_CONSOLE_NEOGEO_POCKET:
