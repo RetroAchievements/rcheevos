c33b992540cb07ad3e9cb4643ec46ebfc37b5eda
Jamiras <jamirasmt@gmail.com>
2020-04-22 11:17:03 -0600

    add rc_console_memory_regions

--- test/rcheevos/test_consoleinfo.c
+++ test/rcheevos/test_consoleinfo.c
@@ -1,10 +1,38 @@
 #include "rconsoles.h"
+#include "rcheevos.h"
 
 #include "../test_framework.h"
 
 static void test_name(int console_id, const char* expected_name)
 {
   ASSERT_STR_EQUALS(rc_console_name(console_id), expected_name);
+}
+
+static void test_memory(int console_id, unsigned expected_total_memory)
+{
+  const rc_memory_regions_t* regions = rc_console_memory_regions(console_id);
+  unsigned total_memory = 0;
+  unsigned max_address = 0;
+  unsigned i;
+  ASSERT_PTR_NOT_NULL(regions);
+
+  if (expected_total_memory == 0)
+  {
+    ASSERT_NUM_EQUALS(regions->num_regions, 0);
+    return;
+  }
+
+  ASSERT_NUM_GREATER(regions->num_regions, 0);
+  for (i = 0; i < regions->num_regions; ++i) {
+    total_memory += (regions->region[i].end_address - regions->region[i].start_address + 1);
+    if (regions->region[i].end_address > max_address)
+      max_address = regions->region[i].end_address;
+
+    ASSERT_PTR_NOT_NULL(regions->region[i].description);
+  }
+
+  ASSERT_NUM_EQUALS(total_memory, expected_total_memory);
+  ASSERT_NUM_EQUALS(max_address, expected_total_memory - 1);
 }
 
 void test_consoleinfo(void) {
@@ -68,6 +96,40 @@ void test_consoleinfo(void) {
   TEST_PARAMS2(test_name, 54, "CassetteVision");
   TEST_PARAMS2(test_name, 55, "Super CassetteVision");
   TEST_PARAMS2(test_name, 56, "Unknown");
+
+  /* memory maps */
+  TEST_PARAMS2(test_memory, RC_CONSOLE_APPLE_II, 0x020000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_ARCADE, 0x000000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_ATARI_2600, 0x000080);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_ATARI_7800, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_ATARI_JAGUAR, 0x200000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_ATARI_LYNX, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_COLECOVISION, 0x000400);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_GAMEBOY, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_GAMEBOY_COLOR, 0x016000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_GAMEBOY_ADVANCE, 0x048000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_GAME_GEAR, 0x002000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_INTELLIVISION, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_MASTER_SYSTEM, 0x002000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_MEGA_DRIVE, 0x020000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_NEOGEO_POCKET, 0x004000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_NINTENDO, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_NINTENDO_64, 0x800000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_NINTENDO_DS, 0x400000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_ORIC, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_PC8800, 0x011000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_PC_ENGINE, 0x42800);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_PLAYSTATION, 0x200000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_POKEMON_MINI, 0x002000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_SATURN, 0x200000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_SEGA_32X, 0x020000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_SEGA_CD, 0x090000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_SG1000, 0x000400);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_SUPER_CASSETTEVISION, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_SUPER_NINTENDO, 0x040000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_WONDERSWAN, 0x010000);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_VECTREX, 0x000400);
+  TEST_PARAMS2(test_memory, RC_CONSOLE_VIRTUAL_BOY, 0x020000);
 
   TEST_SUITE_END();
 }
