079ea2bbb061b8961aa60128b12b10b58f585f3c
Jamiras <jamirasmt@gmail.com>
2023-07-13 08:37:06 -0600

    fix forward reference

--- src/rcheevos/rc_client_internal.h
+++ src/rcheevos/rc_client_internal.h
@@ -243,8 +243,8 @@ int rc_value_contains_memref(const rc_value_t* value, const rc_memref_t* memref)
 /* end runtime.c internals */
 
 /* helper functions for unit tests */
-typedef struct rc_hash_iterator rc_hash_iterator_t;
-rc_hash_iterator_t* rc_client_get_load_state_hash_iterator(rc_client_t* client);
+struct rc_hash_iterator;
+struct rc_hash_iterator* rc_client_get_load_state_hash_iterator(rc_client_t* client);
 /* end helper functions for unit tests */
 
 enum {
