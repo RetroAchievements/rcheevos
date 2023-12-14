0636812eb1483f3a70a7e7bf7434ba8388b69839
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-12-14 08:35:36 -0800

    make this bigger

--- src/rhash/hash.c
+++ src/rhash/hash.c
@@ -1397,7 +1397,7 @@ static int rc_hash_nintendo_3ds(char hash[33], const char* path)
 
     if (verbose_message_callback)
     {
-      char message[64];
+      char message[128];
       snprintf(message, sizeof(message), "Detected NCSD header, seeking to NCCH partition at %08X%08X", (unsigned)(header_offset >> 32), (unsigned)header_offset);
       verbose_message_callback((const char*)header);
     }
