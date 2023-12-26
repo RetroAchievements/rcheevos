c6f14b6c3097749a2be1123642a52ff1d4ecc8cc
Jamiras <jamirasmt@gmail.com>
2023-12-26 07:31:52 -0700

    add formats for FIXEDX, TENS, HUNDREDS, THOUSANDS and UNSIGNED

--- test/rcheevos/test_format.c
+++ test/rcheevos/test_format.c
@@ -22,6 +22,7 @@ void test_format(void) {
   TEST_PARAMS3(test_format_value, RC_FORMAT_VALUE, 12345, "12345");
   TEST_PARAMS3(test_format_value, RC_FORMAT_VALUE, -12345, "-12345");
   TEST_PARAMS3(test_format_value, RC_FORMAT_VALUE, 0xFFFFFFFF, "-1");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_UNSIGNED_VALUE, 0xFFFFFFFF, "4294967295");
   TEST_PARAMS3(test_format_value, RC_FORMAT_SCORE, 12345, "012345");
   TEST_PARAMS3(test_format_value, RC_FORMAT_SECONDS, 45, "0:45");
   TEST_PARAMS3(test_format_value, RC_FORMAT_SECONDS, 345, "5:45");
@@ -38,6 +39,30 @@ void test_format(void) {
   TEST_PARAMS3(test_format_value, RC_FORMAT_FRAMES, 345, "0:05.75");
   TEST_PARAMS3(test_format_value, RC_FORMAT_FRAMES, 12345, "3:25.75");
   TEST_PARAMS3(test_format_value, RC_FORMAT_FRAMES, 1234567, "5h42:56.11");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED1, 0, "0.0");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED1, 1, "0.1");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED1, 1234, "123.4");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED1, -1234, "-123.4");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED2, 0, "0.00");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED2, 1, "0.01");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED2, 1234, "12.34");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED2, -1234, "-12.34");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED3, 0, "0.000");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED3, 1, "0.001");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED3, 1234, "1.234");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_FIXED3, -1234, "-1.234");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_TENS, 0, "0");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_TENS, 1, "10");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_TENS, 1234, "12340");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_TENS, -1234, "-12340");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_HUNDREDS, 0, "0");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_HUNDREDS, 1, "100");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_HUNDREDS, 1234, "123400");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_HUNDREDS, -1234, "-123400");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_THOUSANDS, 0, "0");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_THOUSANDS, 1, "1000");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_THOUSANDS, 1234, "1234000");
+  TEST_PARAMS3(test_format_value, RC_FORMAT_THOUSANDS, -1234, "-1234000");
 
   /* rc_parse_format */
   TEST_PARAMS2(test_parse_format, "VALUE", RC_FORMAT_VALUE);
@@ -50,6 +75,10 @@ void test_format(void) {
   TEST_PARAMS2(test_parse_format, "SCORE", RC_FORMAT_SCORE);
   TEST_PARAMS2(test_parse_format, "POINTS", RC_FORMAT_SCORE);
   TEST_PARAMS2(test_parse_format, "MILLISECS", RC_FORMAT_CENTISECS);
+  TEST_PARAMS2(test_parse_format, "TENS", RC_FORMAT_TENS);
+  TEST_PARAMS2(test_parse_format, "HUNDREDS", RC_FORMAT_HUNDREDS);
+  TEST_PARAMS2(test_parse_format, "THOUSANDS", RC_FORMAT_THOUSANDS);
+  TEST_PARAMS2(test_parse_format, "UNSIGNED", RC_FORMAT_UNSIGNED_VALUE);
   TEST_PARAMS2(test_parse_format, "OTHER", RC_FORMAT_SCORE);
   TEST_PARAMS2(test_parse_format, "INVALID", RC_FORMAT_VALUE);
 
@@ -63,6 +92,17 @@ void test_format(void) {
   TEST_PARAMS2(test_parse_format, "FLOAT6", RC_FORMAT_FLOAT6);
   TEST_PARAMS2(test_parse_format, "FLOAT7", RC_FORMAT_VALUE);
   TEST_PARAMS2(test_parse_format, "FLOAT10", RC_FORMAT_VALUE);
+
+  TEST_PARAMS2(test_parse_format, "FIXED", RC_FORMAT_VALUE);
+  TEST_PARAMS2(test_parse_format, "FIXED0", RC_FORMAT_VALUE);
+  TEST_PARAMS2(test_parse_format, "FIXED1", RC_FORMAT_FIXED1);
+  TEST_PARAMS2(test_parse_format, "FIXED2", RC_FORMAT_FIXED2);
+  TEST_PARAMS2(test_parse_format, "FIXED3", RC_FORMAT_FIXED3);
+  TEST_PARAMS2(test_parse_format, "FIXED4", RC_FORMAT_VALUE);
+  TEST_PARAMS2(test_parse_format, "FIXED5", RC_FORMAT_VALUE);
+  TEST_PARAMS2(test_parse_format, "FIXED6", RC_FORMAT_VALUE);
+  TEST_PARAMS2(test_parse_format, "FIXED7", RC_FORMAT_VALUE);
+  TEST_PARAMS2(test_parse_format, "FIXED10", RC_FORMAT_VALUE);
 
   TEST_SUITE_END();
 }
