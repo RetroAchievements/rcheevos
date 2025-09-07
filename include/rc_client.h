abd568a3f050975b36d7acaac52ace400e2f4849
Jamiras <32680403+Jamiras@users.noreply.github.com>
2025-09-07 17:37:44 -0600

    add rc_client_get_user_subset_summary (#455)

--- include/rc_client.h
+++ include/rc_client.h
@@ -211,6 +211,10 @@ typedef struct rc_client_user_game_summary_t {
 
   uint32_t points_core;
   uint32_t points_unlocked;
+
+  /* minimum version: 12.1 */
+  time_t beaten_time;
+  time_t completed_time;
 } rc_client_user_game_summary_t;
 
 /**
@@ -357,6 +361,8 @@ typedef struct rc_client_subset_t {
 } rc_client_subset_t;
 
 RC_EXPORT const rc_client_subset_t* RC_CCONV rc_client_get_subset_info(rc_client_t* client, uint32_t subset_id);
+
+RC_EXPORT void RC_CCONV rc_client_get_user_subset_summary(const rc_client_t* client, uint32_t subset_id, rc_client_user_game_summary_t* summary);
 
 /*****************************************************************************\
 | Fetch Game Hashes                                                           |
